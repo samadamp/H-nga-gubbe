28de19ebcfdeafab3b88ee450ec12dcb37572b09
samadamp <samadampankhurst@gmail.com>
2023-10-10 11:44:49 +0200

    updateded

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-
-/* const arrayStrings = ["apple", "banana", "cherry"];
-const letterToFind = "a";
-
-function letterInArray(letter, arrayOfStrings) {
-    
-    for (let i = 0; i < arrayOfStrings.length; i++) {
-      const currentString = arrayOfStrings[i];
-      
-      if (currentString.includes(letter)) {
-        return true;
-      }
-    }
-    return false;
-  }
- 
-  if (letterInArray(letterToFind, arrayStrings)) {
-    console.log(`The letter "${letterToFind}" is in the array.`);
-  } else {
-    console.log(`The letter "${letterToFind}" is not in the array.`);
-  }
- */
-
-
-
-  
-
 // Ordlistan
 const wordList = ["apple", "banana", "cherry", "date", "elderberry"];
 
@@ -118,4 +91,3 @@ function resetGame() {
 guessButton.addEventListener("click", makeGuess);
 
 // Lyssna på tangenttryckning för att göra en giss
-
